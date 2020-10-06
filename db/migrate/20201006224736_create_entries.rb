7490ef708c2dc1f017fb1846c8c3b05ebc8c7d64
kayla <kkingston09@gmail.com>
2020-10-06 23:50:50 +0100

    initial commit

---
+++ db/migrate/20201006224736_create_entries.rb
@@ -0,0 +1,12 @@
+class CreateEntries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :entries do |t|
+      t.text :content
+      t.integer :user_id
+      t.datetime :date
+      t.string :word
+
+      t.timestamps
+    end
+  end
+end
